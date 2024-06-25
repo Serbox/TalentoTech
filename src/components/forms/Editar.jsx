425f28779f2f2211af12778a29c188dd94e536a2
SilkHood <andres7.7mac@gmail.com>
2024-06-25 07:09:40 -0500

    Update
    Arreglo especificos

--- src/components/forms/Editar.jsx
+++ src/components/forms/Editar.jsx
@@ -112,7 +112,7 @@ const Editar = () => {
                                     <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
                                         <div class="flex items-center ps-3">
                                             <input id="horizontal-list-radio-id" type="radio" value="" name="list-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 dark:focus:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500"/>
-                                            <label for="horizontal-list-radio-id" class="w-full py-3 ms-2 text-sm font-medium text-gray-900">Profesor</label>
+                                            <label for="horizontal-list-radio-id" class="w-full py-3 ms-2 text-sm font-medium text-gray-900">Docente</label>
                                         </div>
                                     </li>
                                     <li class="w-full border-b border-gray-200 sm:border-b-0 sm:border-r dark:border-gray-600">
@@ -123,8 +123,10 @@ const Editar = () => {
                                     </li>
                                 </ul>
                             </div>
-                            <button type="submit" class="btn btn-primary">Actualizar</button>
+                              <div className="d-flex flex-row justify-content-evenly pt-3">
+                                <button type="submit" class="btn btn-primary">Actualizar</button>
                                 <a href="/" class="btn btn-outline-warning">Cancelar</a>
+                              </div>
                           </div>
                         </div>
                       </div>
