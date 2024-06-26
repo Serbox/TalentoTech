b938fa4e7a34391f02faa40b8e572cb60ed24c4e
SilkHood <andres7.7mac@gmail.com>
2024-06-26 18:49:50 -0500

    Banner

--- src/components/Info-principal/Info-principal.js
+++ src/components/Info-principal/Info-principal.js
@@ -6,6 +6,10 @@ import ImgHackatones from '../../IMG/hackatones.svg';
 import ImgPQR from '../../IMG/PQR.svg';
 import ImgBootcamps from '../../IMG/Bootcamps.svg';
 import ImgUsuario from '../../IMG/Usuario.svg';
+import Carousel from 'react-bootstrap/Carousel';
+
+import ImgCarrousel from '../../IMG/carrusel.jpg'
+
 import { Link } from 'react-router-dom';
 
 const Info = () => {
@@ -101,48 +105,38 @@ const Info = () => {
     }, []);
 
 
+
     return (
         <>
-            <div className='carrousel-student'>
-                <div className='flex flex-row'>
-                    <div id="default-carousel" class="relative w-full" data-carousel="slide">
-                        <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
-                            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                                <img src="/docs/images/carousel/carousel-1.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..."/>
-                            </div>
-                        </div>
-                        <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-                            <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-                            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-                            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-                            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-                            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-                        </div>
-                        <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-                            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-                                </svg>
-                                <span class="sr-only">Previous</span>
-                            </span>
-                        </button>
-                        <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-                            <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-                                </svg>
-                                <span class="sr-only">Next</span>
-                            </span>
-                        </button>
+            <div className='carrousel-student  p-12'>
+                <div className='flex flex-row p-6'>
+                    <div className='max-w-sm max-h-96'>
+                        <Carousel>
+                            <Carousel.Item>
+                                <img src="https://www.mintic.gov.co/portal/715/articles-333597_foto_marquesina.jpg" className='h-auto' alt="..." />
+                                <Carousel.Caption>
+                                <h3>First slide label</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img src="https://www.mintic.gov.co/portal/715/articles-333040_foto_marquesina.jpg" alt="..." />
+                                <Carousel.Caption>
+                                <h3>Third slide label</h3>
+                                <p>
+                                    Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                                </p>
+                                </Carousel.Caption>
+                            </Carousel.Item>
+                        </Carousel>
                     </div>
-                    <div className='max-w-sm'>
-                        <h1>Novedades en talento tech</h1>
-                        <p> Haz tus propias noticias</p>
+                    <div className='max-w-xl p-6 flex flex-col align-middle justify-center'>
+                        <h1 className='font-bold text-gray-700 text-3xl'>Novedades en Talento Tech</h1>
+                        <p className='font-bold text-blue-500 text-xl'> Crea tus propios novedades </p>
 
                         <a className=''> Crea tus propias noticias</a>
-                        <a className=''> Como crear tu noticia</a>
+                        <a className=''> Crea tus propias Novedades</a>
                     </div>
-
                 </div>
                 <ul className='grid grid-cols-3 grid-rows-2 pb-16'>
                         <li className='flex flex-row justify-between'>
