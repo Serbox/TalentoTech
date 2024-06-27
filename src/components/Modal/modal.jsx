56809c24232583ed944d90a004ea1380f7cfdf12
SilkHood <andres7.7mac@gmail.com>
2024-06-26 23:11:40 -0500

    Modales

--- src/components/Modal/modal.jsx
+++ src/components/Modal/modal.jsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './modal.css';
+import { Button } from 'bootstrap';
+
 function modal({Form, Title}) {
   return (
     /*<div className="content_editar_usuario">
@@ -16,26 +18,14 @@ function modal({Form, Title}) {
       </div>
     </div>*/
     <>
-      <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-        Launch demo modal
+      <button className='bg-gray-400 rounded-sm text-white font-bold'>
+        {Title}
       </button>
-
-      <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <div class="modal-body">
-              ...
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-              <button type="button" class="btn btn-primary">Save changes</button>
-            </div>
-          </div>
+      <div className='background_modal fixed inset-0 flex justify-center items-center'>
+        <div className='modal bg-white rounded flex flex-col justify-center items-center'>
+          {Form}
         </div>
+
       </div>
     </>
   );
