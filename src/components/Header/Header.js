7dd3498680cec4184ae33a514cd4499e4f8f78ba
SilkHood <andres7.7mac@gmail.com>
2024-06-28 00:00:50 -0500

    Fin update vista NAV

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,9 @@ import LogoTalento from '../../IMG/TalentoTech.png'
 import { Link } from 'react-router-dom'
 const Header = () => {
     let [isOpen, setisOpen] = useState(false)
-    let [isOpend, setisOpend] = useState(false)
+    let [isOpenUser, setisOpenUser] = useState(false)
+    let [isOpenOptions, setisOpenOptions] = useState(false)
+
 
   return (
     <>
@@ -28,35 +30,58 @@ const Header = () => {
                     <li className='font-semibold my-6 md:my-0 md:ml-8'>
                         <a href='/' className='block font-semibold text-white text-lg decoration-transparent p-0 hover:text-xl hover:font-bold'><i className="bi bi-house-fill px-2"> </i>Inicio</a>
                     </li>
-                    <li className='font-semibold my-6 md:my-0 md:ml-8'>
-                        <a href='/' className='block font-semibold text-white text-lg decoration-transparent p-0 hover:text-xl hover:font-bold'><i className="bi bi-grid-fill px-2"></i>Opciones</a>
+                    <li className='relative flex flex-col font-semibold my-6 md:my-0 md:ml-8 hover:bg-blue-950 p-2 rounded-xl hover:shadow'>
+                        <a href='#' onClick={() => setisOpenOptions((prev) => !prev)} className='w-full flex justify-between font-semibold text-white text-lg decoration-transparent p-0 gap-2 items-center px-3 hover:text-lg hover:font-bold tracking-wider'>
+                            Opciones
+                            {!isOpenOptions ?(
+                                <i className="bi bi-grid-fill px-2"></i>
+                            ):(
+                                <i class="bi bi-ui-checks-grid px-2 "></i>
+                            )}
+                            {isOpenOptions && (
+                                <div className='bg-blue-950 absolute top-14 left-0 flex flex-col items-start rounded-lg p-2 w-full shadow-black'>
+                                    <div className='bg-slate-50 w-full rounded-sm text-center mt-1 hover:bg-blue-950'>
+                                        <a href='/' className='text-gray-700 text-base font-bold w-100 hover:text-white hover:text-bases hover:font-bold  '><i class="bi bi-people-fill px-1"></i>Usuario</a>
+                                    </div>
+                                    <div className='bg-slate-50 w-full rounded-sm text-center mt-1 hover:bg-blue-950'>
+                                        <a href='/' className='text-gray-700 text-base font-bold w-100 hover:text-white hover:text-bases hover:font-bold  '><i class="bi bi-calendar-event-fill px-1"></i>Sesiones</a>
+                                    </div>
+                                    <div className='bg-slate-50 w-full rounded-sm text-center mt-1 hover:bg-blue-950'>
+                                        <a href='/' className='text-gray-700 text-base font-bold w-100 hover:text-white hover:text-bases hover:font-bold  '><i class="bi bi-newspaper px-1"></i>Noticias</a>
+                                    </div>
+                                    <div className='bg-slate-50 w-full rounded-sm text-center mt-1 hover:bg-blue-950'>
+                                        <a href='/' className='text-gray-700 text-base font-bold w-100 hover:text-white hover:text-bases hover:font-bold  '><i class="bi bi-inboxes-fill px-1"></i>Bootcamp </a>
+                                    </div>
+                                </div>
+                            )}
+                        </a>
                     </li>
                     <li className='font-semibold my-6 md:my-0 md:ml-8'>
                         <a href='/' className='block font-semibold text-white text-lg decoration-transparent p-0 hover:text-xl hover:font-bold'><i className="bi bi-question-circle-fill px-2"> </i>Ayuda</a>
                     </li>
                     <li className=" relative flex flex-col items-center font-semibold my-7 md:my-0 md:ml-8 bg-blue-950  border-b-4  border-sky-950 p-2 rounded-xl shadow">
-                            <a href='#' onClick={() => setisOpend((prev) => !prev)} className='w-full flex justify-between font-sbold text-white text-lg decoration-transparent p-0 gap-2 items-center px-3 hover:text-lg hover:font-semibold tracking-wider'>
+                            <a href='#' onClick={() => setisOpenUser((prev) => !prev)} className='w-full flex justify-between font-semibold text-white text-lg decoration-transparent p-0 gap-2 items-center px-3 hover:text-lg hover:font-semibold tracking-wider'>
                                 <div className='rounded-full bg-gray-400 p-1 text-sm hover:text-sm hover:font-bold'>
                                     SM
                                 </div>
                                 Sylvanas
-                                {!isOpend ?(
+                                {!isOpenUser ?(
                                     <i class="bi bi-caret-down-fill"></i>
                                 ) :(
                                     <i class="bi bi-caret-up-fill"></i>
                                 )}
-                                {isOpend &&(
+                                {isOpenUser &&(
                                     <div className='bg-blue-950 absolute top-14 left-0 flex flex-col items-start rounded-lg p-2 w-full shadow-black'>
                                         <div className='flex flex-row w-full h-full items-center bg-slate-50 hover:bg-slate-200 rounded-lg p-1'>
                                             <div className='flex flex-col px-2 w-full '>
-                                                    <h4 className='text-gray-700 text-lg font-bold p-0 m-0 text-center'>Sylvanas</h4>
-                                                    <h4 className='text-gray-700 text-lg font-bold p-0 m-0 text-center'>Hayase</h4>
-                                                <div className='text-base text-center text-sky-500 font-bold'>
+                                                    <h4 className='text-gray-700 text-xl font-bold p-0 m-0 text-center'>Sylvanas</h4>
+                                                    <h4 className='text-gray-700 text-xl font-bold p-0 m-0 text-center'>Hayase</h4>
+                                                <div className='text-base text-center text-sky-700 font-bold'>
                                                     Sylvanas@
                                                 </div>
 
                                             </div>
-                                            <div className='bg-gray-300 rounded-full p-0 mx-2'>
+                                            <div className='bg-gray-300 rounded-full p-0 mx-2 border-2 border-gray-400 shadow'>
                                                 <h5 className='p-2 text-center font-medium'>SM</h5>
                                             </div>
                                         </div>
